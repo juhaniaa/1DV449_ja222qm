da772f89831b1c656c7d1ec4fed5cde790fb0561
ja222qm <ja222qm@student.lnu.se>
2014-11-10 21:13:10 +0200

    Cleaner

--- Laboration01/L01.php
+++ Laboration01/L01.php
@@ -24,116 +24,55 @@ if($dom->loadHTML($data)){
 	// varje "blog"-element i listan
 	foreach ($courses as $course) {
 		
+		$noInfo = "no information";
+		
+		/* Name */
+		$cName = $noInfo;
 		$cName = $course->nodeValue;
+		
+		/* URL */
+		$cUrl = $noInfo;
 		$cUrl = $course->getAttribute("href");
 		
 		$html = file_get_html($cUrl);
 		
+		/* Code */
+		$cCode = $noInfo;
 		$cNode = $html->find('div[id=header-wrapper] li', 2);
-		$cCode = $cNode->plaintext;
-				
-		$sNode = $html->find("ul.sub-menu li a[href*=prod]");
-		
-		foreach ($sNode as $sUrl) {
-			$cSyllabus = $sUrl->href;			
+		if($cNode){
+			$cCode = $cNode->plaintext;
 		}
 		
-		$iNode = $html->find("div.entry-content", 0);		
-		$cInfo = $iNode->plaintext;
+		/* Syllabus */
+		$cSyllabus = $noInfo;
+		$sNode = $html->find("ul.sub-menu li a[href*=prod]", 0);	
+		if($sNode){
+			$cSyllabus = $sNode->href;
+		}			
 		
+		/* Information */
+		$cInfo = $noInfo;
+		$iNode = $html->find("div.entry-content", 0);	
+		if($iNode){	
+			$cInfo = $iNode->plaintext;
+		}
+		
+		/* Latest post header */
+		$cPostHeader = $noInfo;
 		$postHeaderNode = $html->find("#content header.entry-header h1 a", 0);
 		if($postHeaderNode){
 			$cPostHeader = $postHeaderNode->plaintext;	
-		} else{
-			$cPostHeader = "no information";
 		}
 		
-		
+		/* Latest post writer and date/time */
+		$cWriter = $noInfo;
 		$postWriterNode = $html->find("#content header.entry-header p.entry-byline", 0);
 		if($postWriterNode){
 			$cWriter = $postWriterNode->plaintext;
-		} else{
-			$cWriter = "no information";
 		}
-		
-		
-		
+
 		$html->clear();
 		unset($html);
-		
-		/*foreach ($codeNode as $code) {
-			echo $code->plaintext;	
-		}
-		
-		//var_dump($codeNode);
-		
-		/*
-		// gå in på varje länk för att hitta ytterligare info
-		$cData = curl_get_request($cUrl);
-		
-		$libDom = HTML5_Parser::parse($cData);
-		var_dump($libDom);
-		echo "thats doc</br>";
-		
-		$libXpath = new DOMXPath($libDom);
-			
-		$libNodeList = $libXpath->query("//body/div[@id = 'page']//li");
-		
-		foreach ($libNodeList as $specNode) {
-			var_dump($specNode);
-			echo "</br>";
-			var_dump($specNode->parentNode->parentNode->attributes);
-			var_dump($specNode->nodeValue);
-		}
-		
-		var_dump($libNodeList);
-		
-		
-		*/
-		
-		//var_dump($libDom->textContent);
-		//$nodelist = HTML5_Parser::parseFragment('<li></li>', 'header');
-		//var_dump($nodelist);
-		//echo "thats Nodelist</br>";
-		
-		//$node = $nodelist->item(0)->nodeValue;
-		//var_dump($node);
-		
-		//foreach ($nodelist as $testNode) {
-			//var_dump($testNode->nodeValue);
-		//}
-		
-		/*
-		$cDom = new DOMDocument();
-		//libxml_use_internal_errors(true);
-		
-		$cElement = $cDom->getElementById("header-wrapper");
-		var_dump($cElement);
-		echo $cElement;
-		
-		if($cDom->loadHTML($cData)){
-			$cXpath = new DOMXPath($cDom);
-			
-			echo $cXpath;
-			
-			//$cCode = $cXpath->query("/div[@id = 'page']/div[@id = 'header-wrapper']/ul//li");
-			//$cCode = $cXpath->query("//body/div[@id = 'page']/div[@id = 'header-wrapper']//li");
-			$cCode = $cXpath->query("//body/div[@id = 'page']//li");
-			var_dump($cCode);
-			
-			foreach ($cCode as $item) {
-				$test = $item->nodeValue;
-				var_dump($test);
-				echo "hej";
-			}
-			
-			//$cCode = $cCode->item(0)->nodeValue;
-			//var_dump($cCode);
-		}
-		//libxml_use_internal_errors(false);
-		 * 
-		 * 
-		 */
 		
 		$html = "<li><ul>
 		<li>Name: " . $cName . "</li>
@@ -146,8 +85,6 @@ if($dom->loadHTML($data)){
 		echo $html;
 	}
 }
-
-
 
 else {
 	die("HTML läsningen misslyckades");
